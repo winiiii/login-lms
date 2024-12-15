9bd8e1fd43df4ae1ef6fd19319ccec1e11bca9a9
saivinay11 <vinaybenten@gmail.com>
2024-12-15 13:47:51 +0000

    completed web app script

--- script.sh
+++ script.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
 echo " deploying application on INGINX"
+
+
+echo "Updating System"
+sudo apt update -y
+
+echo "Installing Utilities"
+sudo apt install -y zip unzip
+
+echo "Installing NGINX Web Server"
+sudo apt install -y nginx
+
+echo "Remove Sample Pages"
+sudo rm -rf /var/www/html
+
+echo "Clone Login App"
+sudo git clone https://github.com/winiiii/login-lms.git /var/www/html
+
+echo " Deployed web apps on NIGNX"
+
+
+
+
